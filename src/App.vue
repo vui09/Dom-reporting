a42f27ebcf6b44315bb9c1715d859dcd15c46595
Julia V <vid090293@yandex.ru>
2023-04-25 12:44:21 +0300

    Верстка таблицы

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,211 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section class="region-credit">
+    <div class="region-credit__container">
+      <h1 class="region-credit__title">Единая отчетность по региональным программам льготного ипотечного кредитования</h1>
+      <div class="region-credit__date-wrapper">
+        <div class="region-credit__date">
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10ZM18 10C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10ZM9.338 11.0101C9.31933 11.1594 9.31 11.3227 9.31 11.5001V11.9201H10.542V11.6401C10.542 11.5187 10.5513 11.4067 10.57 11.3041C10.598 11.1921 10.64 11.0847 10.696 10.9821C10.7613 10.8701 10.8453 10.7581 10.948 10.6461C11.0507 10.5247 11.1813 10.3987 11.34 10.2681C11.536 10.1001 11.7273 9.92273 11.914 9.73606C12.1007 9.54006 12.264 9.33939 12.404 9.13406C12.5533 8.91939 12.67 8.69539 12.754 8.46206C12.838 8.22873 12.88 7.98139 12.88 7.72006C12.88 7.33739 12.81 6.98739 12.67 6.67006C12.5393 6.34339 12.3433 6.06339 12.082 5.83006C11.83 5.58739 11.522 5.40073 11.158 5.27006C10.794 5.13006 10.388 5.06006 9.94 5.06006C9.492 5.06006 9.086 5.13006 8.722 5.27006C8.358 5.40073 8.04533 5.58739 7.784 5.83006C7.532 6.06339 7.336 6.34339 7.196 6.67006C7.06533 6.98739 7 7.33739 7 7.72006H8.26C8.26 7.27206 8.40933 6.90339 8.708 6.61406C9.00667 6.32473 9.41733 6.18006 9.94 6.18006C10.4627 6.18006 10.8733 6.32473 11.172 6.61406C11.4707 6.90339 11.62 7.27206 11.62 7.72006C11.62 8.02806 11.5173 8.31739 11.312 8.58806C11.1067 8.85873 10.808 9.16206 10.416 9.49806C10.1733 9.70339 9.97733 9.89006 9.828 10.0581C9.688 10.2261 9.576 10.3894 9.492 10.5481C9.41733 10.6974 9.366 10.8514 9.338 11.0101ZM9.24 14.8601C9.436 15.0467 9.66933 15.1401 9.94 15.1401C10.2107 15.1401 10.4393 15.0467 10.626 14.8601C10.822 14.6641 10.92 14.4307 10.92 14.1601C10.92 13.8801 10.822 13.6467 10.626 13.4601C10.4393 13.2734 10.2107 13.1801 9.94 13.1801C9.66933 13.1801 9.436 13.2734 9.24 13.4601C9.05333 13.6467 8.96 13.8801 8.96 14.1601C8.96 14.4307 9.05333 14.6641 9.24 14.8601Z" fill="#999999"></path></svg>
+          Отчет по данным на 19 апреля 2023 г.
+        </div>
+        <a class="region-credit__link" href="#" title="Открыть отчёт"> Подробнее...</a>
+      </div>
+      <div class="region-credit__tab-wrap">
+        <div class="region-credit__filters-wrap">
+          <section class="rc-filters">
+            <form>
+              <div class="rc-filters__fieldgroup">
+                <div class="rc-filters__select">
+                  <button class="rc-filters-select__value">
+                    <span class="rc-filters-select__name">Федеральный округ/Регион</span>
+                  </button>
+                </div>
+                <div class="rc-filters-select__list-wrap">
+                  <div class="rc-filters-select__search">
+                    <input type="text" id="selectCheckboxSearch" name="selectCheckboxSearch" placeholder="Поиск по регионам">
+                  </div>
+                  <ul class="rc-filters-select__list">
+                    <li>Центральный ФО</li>
+                    <li>Южный ФО</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="rc-filters__fieldgroup">
+
+              </div>
+              <div class="rc-filters__fieldgroup">
+
+              </div>
+            </form>
+          </section>
+        </div>
+      </div>
+      <section class="rc-table-tabs">
+        <div class="rc-table-tabs__wrap">
+          <button class="rc-tab active">Таблицами</button>
+          <button class="rc-tab">На графиках</button>
+        </div>
+      </section>
+      <section class="rc-table">
+        <h2 class="rc-table__title">
+          Сводные показатели реализации региональных программ льготного ипотечного кредитования
+        </h2>
+        <div class="rc-table__table--wrap">
+          <div class="rc-table__table">
+            <div class="rc-table__thead">
+              <div class="rc-table__row">
+                <div class="rc-table__col rc-table__col-head">
+                  <button class="rc-table__sort">
+                    <span>Регионы</span>
+                    <svg class="increase" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="2" width="16" height="2" fill="#999999"></rect><rect y="7" width="12" height="2" fill="#999999"></rect><rect y="12" width="8" height="2" fill="#999999"></rect></svg>
+                    <svg class="decrease" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="2" width="8" height="2" fill="#999999"></rect><rect y="7" width="12" height="2" fill="#999999"></rect><rect y="12" width="16" height="2" fill="#999999"></rect></svg>
+                  </button>
+                </div>
+                <div class="rc-table__col rc-table__col-head">
+                  <button class="rc-table__sort">
+                    <span>Количество банков - участников региональных программ, шт.</span>
+                    <svg class="increase" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="2" width="16" height="2" fill="#999999"></rect><rect y="7" width="12" height="2" fill="#999999"></rect><rect y="12" width="8" height="2" fill="#999999"></rect></svg>
+                    <svg class="decrease" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="2" width="8" height="2" fill="#999999"></rect><rect y="7" width="12" height="2" fill="#999999"></rect><rect y="12" width="16" height="2" fill="#999999"></rect></svg>
+                  </button>
+                </div>
+                <div class="rc-table__col rc-table__col-head">
+                  <button class="rc-table__sort">
+                    <span>Количество выданных кредитов, тыс</span>
+                    <svg class="increase" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="2" width="16" height="2" fill="#999999"></rect><rect y="7" width="12" height="2" fill="#999999"></rect><rect y="12" width="8" height="2" fill="#999999"></rect></svg>
+                    <svg class="decrease" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="2" width="8" height="2" fill="#999999"></rect><rect y="7" width="12" height="2" fill="#999999"></rect><rect y="12" width="16" height="2" fill="#999999"></rect></svg>
+                  </button>
+                </div>
+                <div class="rc-table__col rc-table__col-head">
+                  <button class="rc-table__sort">
+                    <span>Объем выданных кредитов, млн ₽</span>
+                    <svg class="increase" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="2" width="16" height="2" fill="#999999"></rect><rect y="7" width="12" height="2" fill="#999999"></rect><rect y="12" width="8" height="2" fill="#999999"></rect></svg>
+                    <svg class="decrease" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="2" width="8" height="2" fill="#999999"></rect><rect y="7" width="12" height="2" fill="#999999"></rect><rect y="12" width="16" height="2" fill="#999999"></rect></svg>
+                  </button>
+                </div>
+                <div class="rc-table__col rc-table__col-head">
+                  <button class="rc-table__sort">
+                    <span>Объем выплат средств субсидии банкам, млн ₽</span>
+                    <svg class="increase" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="2" width="16" height="2" fill="#999999"></rect><rect y="7" width="12" height="2" fill="#999999"></rect><rect y="12" width="8" height="2" fill="#999999"></rect></svg>
+                    <svg class="decrease" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="2" width="8" height="2" fill="#999999"></rect><rect y="7" width="12" height="2" fill="#999999"></rect><rect y="12" width="16" height="2" fill="#999999"></rect></svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="rc-table__tbody">
+              <div class="rc-table__row rc-table__row--title">
+                <div class="rc-table__col">
+                  <span class="rc-table__text rc-table__text--title">Всего</span>
+                </div>
+                <div class="rc-table__col">
+                  <span class="rc-table__text">4</span>
+                </div>
+                <div class="rc-table__col">
+                  <span class="rc-table__text">6</span>
+                </div>
+                <div class="rc-table__col">
+                  <span class="rc-table__text">884,56</span>
+                </div>
+                <div class="rc-table__col">
+                  <span class="rc-table__text">543,86</span>
+                </div>
+              </div>
+              <div class="rc-table__row--wrap">
+                <div class="rc-table__row">
+                  <div class="rc-table__col">
+                    <span class="rc-table__text">Ростовская область</span>
+                  </div>
+                  <div class="rc-table__col">
+                    <span class="rc-table__text">43</span>
+                  </div>
+                  <div class="rc-table__col">
+                    <span class="rc-table__text">34265</span>
+                  </div>
+                  <div class="rc-table__col">
+                    <span class="rc-table__text">332</span>
+                  </div>
+                  <div class="rc-table__col">
+                    <span class="rc-table__text">235</span>
+                  </div>
+                </div>
+                <div class="rc-table__subrow-wrap">
+                  <div class="rc-table__row">
+                    <div class="rc-table__col rc-table__col--title">
+                      <span class="rc-table__text">Дисконт</span>
+                      <div class="rc-table__tooltip">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM7.338 9.01006C7.31933 9.15939 7.31 9.32273 7.31 9.50006V9.92006H8.542V9.64006C8.542 9.51873 8.55133 9.40673 8.57 9.30406C8.598 9.19206 8.64 9.08473 8.696 8.98206C8.76133 8.87006 8.84533 8.75806 8.948 8.64606C9.05067 8.52473 9.18133 8.39873 9.34 8.26806C9.536 8.10006 9.72733 7.92273 9.914 7.73606C10.1007 7.54006 10.264 7.33939 10.404 7.13406C10.5533 6.91939 10.67 6.69539 10.754 6.46206C10.838 6.22873 10.88 5.98139 10.88 5.72006C10.88 5.33739 10.81 4.98739 10.67 4.67006C10.5393 4.34339 10.3433 4.06339 10.082 3.83006C9.83 3.58739 9.522 3.40073 9.158 3.27006C8.794 3.13006 8.388 3.06006 7.94 3.06006C7.492 3.06006 7.086 3.13006 6.722 3.27006C6.358 3.40073 6.04533 3.58739 5.784 3.83006C5.532 4.06339 5.336 4.34339 5.196 4.67006C5.06533 4.98739 5 5.33739 5 5.72006H6.26C6.26 5.27206 6.40933 4.90339 6.708 4.61406C7.00667 4.32473 7.41733 4.18006 7.94 4.18006C8.46267 4.18006 8.87333 4.32473 9.172 4.61406C9.47067 4.90339 9.62 5.27206 9.62 5.72006C9.62 6.02806 9.51733 6.31739 9.312 6.58806C9.10667 6.85873 8.808 7.16206 8.416 7.49806C8.17333 7.70339 7.97733 7.89006 7.828 8.05806C7.688 8.22606 7.576 8.38939 7.492 8.54806C7.41733 8.69739 7.366 8.85139 7.338 9.01006ZM7.24 12.8601C7.436 13.0467 7.66933 13.1401 7.94 13.1401C8.21067 13.1401 8.43933 13.0467 8.626 12.8601C8.822 12.6641 8.92 12.4307 8.92 12.1601C8.92 11.8801 8.822 11.6467 8.626 11.4601C8.43933 11.2734 8.21067 11.1801 7.94 11.1801C7.66933 11.1801 7.436 11.2734 7.24 11.4601C7.05333 11.6467 6.96 11.8801 6.96 12.1601C6.96 12.4307 7.05333 12.6641 7.24 12.8601Z" fill="#999999"></path></svg>
+                      </div>
+                    </div>
+                    <div class="rc-table__col">
+                      <span class="rc-table__text">346</span>
+                    </div>
+                    <div class="rc-table__col">
+                      <span class="rc-table__text">243</span>
+                    </div>
+                    <div class="rc-table__col">
+                      <span class="rc-table__text">35346</span>
+                    </div>
+                    <div class="rc-table__col">
+                      <span class="rc-table__text">235</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="rc-table__row--wrap">
+                <div class="rc-table__row">
+                  <div class="rc-table__col">
+                    <span class="rc-table__text">Белгородская область</span>
+                  </div>
+                  <div class="rc-table__col">
+                    <span class="rc-table__text">43</span>
+                  </div>
+                  <div class="rc-table__col">
+                    <span class="rc-table__text">34265</span>
+                  </div>
+                  <div class="rc-table__col">
+                    <span class="rc-table__text">332</span>
+                  </div>
+                  <div class="rc-table__col">
+                    <span class="rc-table__text">235</span>
+                  </div>
+                </div>
+                <div class="rc-table__subrow-wrap">
+                  <div class="rc-table__row">
+                    <div class="rc-table__col rc-table__col--title">
+                      <span class="rc-table__text">Льготная ипотека</span>
+                      <div class="rc-table__tooltip">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8ZM16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8ZM7.338 9.01006C7.31933 9.15939 7.31 9.32273 7.31 9.50006V9.92006H8.542V9.64006C8.542 9.51873 8.55133 9.40673 8.57 9.30406C8.598 9.19206 8.64 9.08473 8.696 8.98206C8.76133 8.87006 8.84533 8.75806 8.948 8.64606C9.05067 8.52473 9.18133 8.39873 9.34 8.26806C9.536 8.10006 9.72733 7.92273 9.914 7.73606C10.1007 7.54006 10.264 7.33939 10.404 7.13406C10.5533 6.91939 10.67 6.69539 10.754 6.46206C10.838 6.22873 10.88 5.98139 10.88 5.72006C10.88 5.33739 10.81 4.98739 10.67 4.67006C10.5393 4.34339 10.3433 4.06339 10.082 3.83006C9.83 3.58739 9.522 3.40073 9.158 3.27006C8.794 3.13006 8.388 3.06006 7.94 3.06006C7.492 3.06006 7.086 3.13006 6.722 3.27006C6.358 3.40073 6.04533 3.58739 5.784 3.83006C5.532 4.06339 5.336 4.34339 5.196 4.67006C5.06533 4.98739 5 5.33739 5 5.72006H6.26C6.26 5.27206 6.40933 4.90339 6.708 4.61406C7.00667 4.32473 7.41733 4.18006 7.94 4.18006C8.46267 4.18006 8.87333 4.32473 9.172 4.61406C9.47067 4.90339 9.62 5.27206 9.62 5.72006C9.62 6.02806 9.51733 6.31739 9.312 6.58806C9.10667 6.85873 8.808 7.16206 8.416 7.49806C8.17333 7.70339 7.97733 7.89006 7.828 8.05806C7.688 8.22606 7.576 8.38939 7.492 8.54806C7.41733 8.69739 7.366 8.85139 7.338 9.01006ZM7.24 12.8601C7.436 13.0467 7.66933 13.1401 7.94 13.1401C8.21067 13.1401 8.43933 13.0467 8.626 12.8601C8.822 12.6641 8.92 12.4307 8.92 12.1601C8.92 11.8801 8.822 11.6467 8.626 11.4601C8.43933 11.2734 8.21067 11.1801 7.94 11.1801C7.66933 11.1801 7.436 11.2734 7.24 11.4601C7.05333 11.6467 6.96 11.8801 6.96 12.1601C6.96 12.4307 7.05333 12.6641 7.24 12.8601Z" fill="#999999"></path></svg>
+                      </div>
+                    </div>
+                    <div class="rc-table__col">
+                      <span class="rc-table__text">346</span>
+                    </div>
+                    <div class="rc-table__col">
+                      <span class="rc-table__text">243</span>
+                    </div>
+                    <div class="rc-table__col">
+                      <span class="rc-table__text">35346</span>
+                    </div>
+                    <div class="rc-table__col">
+                      <span class="rc-table__text">235</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
